111cf7822de2286ed5df98884bbac79503dbf951
Ole <27195190+ohenning@users.noreply.github.com>
2024-05-17 14:15:54 +0200

    /format-versions endpoint

--- src/server/infrastructure/api.routes.ts
+++ src/server/infrastructure/api.routes.ts
@@ -1,13 +1,18 @@
 import { Router } from 'express';
 import AHBController from '../controller/ahb';
+import FormatVersionController from '../controller/formatVersion';
 
 const router = Router();
 
 const ahbController = new AHBController();
-
+const formatVersionController = new FormatVersionController();
 
 router.get('/ahb/:formatVersion/:pruefi', async (req, res, next) => {
   await ahbController.get(req, res).catch((err: Error) => next(err));
+});
+
+router.get('/format-versions', async (req, res, next) => {
+  await formatVersionController.list(req, res).catch((err: Error) => next(err));
 });
 
 router.all('/**', (req, res) => {
