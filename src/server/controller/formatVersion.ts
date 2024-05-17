c0b939097686c86cb69735778cc057c586baa557
Ole Henning <27195190+ohenning@users.noreply.github.com>
2024-05-17 15:20:58 +0200

    Set Json header (#17)

--- src/server/controller/formatVersion.ts
+++ src/server/controller/formatVersion.ts
@@ -9,12 +9,16 @@ export default class FormatVersionController {
 
     public async list(_req: Request, res: Response): Promise<void> {
         const formatVersionEntity = await this.repository.list();
-        res.status(200).send(formatVersionEntity);
+        res.status(200)
+        .setHeader("Content-Type", "application/json")
+        .send(formatVersionEntity);
     }
 
     public async listPruefisByFormatVersion(req: Request, res: Response): Promise<void> {
         const formatVersion = req.params["formatVersion"];
         const pruefis = await this.repository.listPruefisByFormatVersion(formatVersion);
-        res.status(200).send(pruefis);
+        res.status(200)
+        .setHeader("Content-Type", "application/json")
+        .send(pruefis);
     }
 }
