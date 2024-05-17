5430e1217c36b39071a872104172711fb7b19849
Maxim Uhlemann <maxim@bygga.io>
2024-05-17 18:52:18 +0200

    Fix format

--- src/server/infrastructure/azure-blob-storage-client.ts
+++ src/server/infrastructure/azure-blob-storage-client.ts
@@ -1,9 +1,9 @@
-import { BlobServiceClient } from "@azure/storage-blob";
+import { BlobServiceClient } from '@azure/storage-blob';
 
 export function createNewBlobStorageClient(): BlobServiceClient {
-    const connectionString = process.env["AZURE_BLOB_STORAGE_CONNECTION_STRING"];
-    if (!connectionString) {
-        throw new Error("AZURE_BLOB_STORAGE_CONNECTION_STRING is not set");
-    }
-    return BlobServiceClient.fromConnectionString(connectionString);
+  const connectionString = process.env['AZURE_BLOB_STORAGE_CONNECTION_STRING'];
+  if (!connectionString) {
+    throw new Error('AZURE_BLOB_STORAGE_CONNECTION_STRING is not set');
+  }
+  return BlobServiceClient.fromConnectionString(connectionString);
 }
--- src/server/infrastructure/errors.ts
+++ src/server/infrastructure/errors.ts
@@ -1,16 +1,16 @@
-import { ErrorRequestHandler } from "express";
+import { ErrorRequestHandler } from 'express';
 
 export class NotFoundError extends Error {}
 
 export const httpErrorHandler: ErrorRequestHandler = (err, _, res, _next) => {
-    console.error('in error handler');
-    if (err instanceof NotFoundError) {
-        res.status(404).json({
-            error: 'NotFoundError',
-            message: err.message
-        });
-    } else {
-        console.error(err instanceof Error ? err.message : 'Unknown error');
-        res.status(500).send("Internal server error");
-    }
-}
+  console.error('in error handler');
+  if (err instanceof NotFoundError) {
+    res.status(404).json({
+      error: 'NotFoundError',
+      message: err.message,
+    });
+  } else {
+    console.error(err instanceof Error ? err.message : 'Unknown error');
+    res.status(500).send('Internal server error');
+  }
+};
