a5023b7ac8326c85dbaa92c72fd4f1ba13001f8c
kevin <68426071+hf-krechan@users.noreply.github.com>
2024-07-08 11:14:55 +0200

    Add Pulumi Setup (#91)
    * pulumi new
    
    * 🚧wip
    
    * 🙈 pulumi/.venv
    
    * 🚧 WIP Gh action
    
    * ⚙️ Change trigger
    
    * remove unused imports
    
    * 🖤 apply black
    
    * 🚧 wip
    
    * update pulumi dev config
    
    * read ghcr_token from ahb tabellen config
    
    * 🚧📝 WIP PulumiDocumentation
    
    * 🐳🙈 add pulumi folder to dockerignore
    
    * 🚧🎨 clean up
    
    * Fix typo in comment
    
    * 🎉 starting container
    
    * ⚙️ update dev config
    
    * 🔥 remove unused files
    
    * 🔥 remove uneccessary lines
    
    * set variable for format_version_container_name
    
    * 📝 add documentation
    
    * Add info about python virtual environment
    
    * 🙈 Update gitignore
    
    * ➖ remove unused dependencies
    
    * apply prettier
    
    ---------
    
    Co-authored-by: Leon Haffmans <leon.haffmans@hochfrequenz.de>

---
+++ pulumi/__main__.py
@@ -0,0 +1,129 @@
+"""
+Pulumi program that deploys a containerized web service to Azure Container Instances.
+"""
+
+import pulumi_azure_native as azure_native
+import pulumi
+
+
+# Import the program's configuration settings.
+config = pulumi.Config()
+
+image_name = config.get("imageName")
+image_tag = config.get("imageTag")
+assert image_name, "imageName must be set"
+assert image_tag, "imageTag must be set"
+
+image_name_with_tag = f"{image_name}:{image_tag}"
+ghcr_token = config.require_secret("ghcr_token")
+assert ghcr_token, "ghcr_token must be set"
+
+ahb_blob_container_name = config.get("ahbBlobContainerName")
+assert ahb_blob_container_name, "ahbBlobContainerName must be set"
+
+format_version_container_name = config.get("formatVersionContainerName")
+assert format_version_container_name, "formatVersionContainerName must be set"
+
+container_port = config.get_int("containerPort")
+assert container_port, "containerPort must be set"
+
+cpu = config.get_int("cpu", 1)
+memory = config.get_int("memory", 2)
+
+# Create an Azure Resource Group
+resource_group = azure_native.resources.ResourceGroup("ahb-tabellen")
+
+# Create an Azure Storage Account
+storage_account = azure_native.storage.StorageAccount(
+    # Storage account name must be between 3 and 24 characters in length and use numbers and
+    # lower-case letters only.
+    "ahbtabellen",
+    resource_group_name=resource_group.name,
+    sku=azure_native.storage.SkuArgs(
+        name=azure_native.storage.SkuName.STANDARD_LRS,
+    ),
+    kind=azure_native.storage.Kind.STORAGE_V2,
+)
+
+# Create an Azure Blob Container
+blob_container = azure_native.storage.BlobContainer(
+    ahb_blob_container_name,
+    resource_group_name=resource_group.name,
+    account_name=storage_account.name,
+    public_access=azure_native.storage.PublicAccess.NONE,
+)
+
+# Securely retrieve the primary storage account key
+primary_key = pulumi.Output.all(resource_group.name, storage_account.name).apply(
+    lambda args: azure_native.storage.list_storage_account_keys(
+        resource_group_name=args[0], account_name=args[1]
+    )
+    .keys[0]
+    .value
+)
+
+# Generate the connection string securely
+azure_blob_storage_connection_string = pulumi.Output.all(
+    storage_account.name, primary_key
+).apply(
+    lambda args: f"DefaultEndpointsProtocol=https;AccountName={args[0]};AccountKey={args[1]};EndpointSuffix=core.windows.net"
+)
+
+
+# Resource requirements for the container
+resource_requirements = azure_native.containerinstance.ResourceRequirementsArgs(
+    requests=azure_native.containerinstance.ResourceRequestsArgs(
+        cpu=0.5,
+        memory_in_gb=1.5,
+    )
+)
+
+# Port configuration for the container
+container_ports = [
+    azure_native.containerinstance.ContainerPortArgs(port=container_port)
+]
+
+ip_address_ports = [azure_native.containerinstance.PortArgs(port=container_port)]
+
+# Define environment variables for the container
+environment_variables = [
+    azure_native.containerinstance.EnvironmentVariableArgs(
+        name="PORT", value=str(container_port)
+    ),
+    azure_native.containerinstance.EnvironmentVariableArgs(
+        name="AZURE_BLOB_STORAGE_CONNECTION_STRING",
+        value=azure_blob_storage_connection_string,
+    ),
+    azure_native.containerinstance.EnvironmentVariableArgs(
+        name="AHB_CONTAINER_NAME", value=ahb_blob_container_name
+    ),
+    azure_native.containerinstance.EnvironmentVariableArgs(
+        name="FORMAT_VERSION_CONTAINER_NAME", value=format_version_container_name
+    ),
+]
+
+# Container group definition
+container_group = azure_native.containerinstance.ContainerGroup(
+    "ahb-tabellen",
+    resource_group_name=resource_group.name,
+    os_type="Linux",
+    containers=[
+        azure_native.containerinstance.ContainerArgs(
+            name="ahb-tabellen-container",
+            image=image_name_with_tag,
+            resources=resource_requirements,
+            ports=container_ports,
+            environment_variables=environment_variables,
+        )
+    ],
+    ip_address=azure_native.containerinstance.IpAddressArgs(
+        ports=ip_address_ports, type="Public"
+    ),
+    image_registry_credentials=[
+        azure_native.containerinstance.ImageRegistryCredentialArgs(
+            server="ghcr.io",
+            username="hf-krechan",
+            password=ghcr_token,
+        ),
+    ],
+)
