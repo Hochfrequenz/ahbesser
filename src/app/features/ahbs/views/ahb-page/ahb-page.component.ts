ca24b805806a3ec8f7c2e99696f09311949fa016
olli <144932831+OLILHR@users.noreply.github.com>
2024-08-02 17:46:46 +0200

    ✨ Add EDIFACT format to AHB page (#145)
    * Add EDIFACT format to AHB page
    
    * Remove unused constant
    
    * Specify UTILMD Strom/Gas
    
    Co-authored-by: kevin <68426071+hf-krechan@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: kevin <68426071+hf-krechan@users.noreply.github.com>

--- src/app/features/ahbs/views/ahb-page/ahb-page.component.ts
+++ src/app/features/ahbs/views/ahb-page/ahb-page.component.ts
@@ -1,5 +1,6 @@
 import {
   Component,
+  computed,
   ElementRef,
   effect,
   input,
@@ -42,6 +43,7 @@ export class AhbPageComponent {
   scroll = viewChild<ElementRef>('scroll');
 
   searchQuery = signal<string | undefined>('');
+  edifactFormat = computed(() => this.getEdifactFormat(this.pruefi()));
 
   ahb$?: Observable<Ahb>;
   lines$?: Observable<Ahb['lines']>;
@@ -56,6 +58,35 @@ export class AhbPageComponent {
         .pipe(shareReplay());
       this.lines$ = this.ahb$.pipe(map((ahb) => ahb.lines));
     });
+  }
+
+  // mapping provided by mig_ahb_utility_stack
+  private getEdifactFormat(pruefi: string): string {
+    const mapping: { [key: string]: string } = {
+      '99': 'APERAK',
+      '29': 'COMDIS',
+      '21': 'IFTSTA',
+      '23': 'INSRPT',
+      '31': 'INVOIC',
+      '13': 'MSCONS',
+      '39': 'ORDCHG',
+      '17': 'ORDERS',
+      '19': 'ORDRSP',
+      '27': 'PRICAT',
+      '15': 'QUOTES',
+      '33': 'REMADV',
+      '35': 'REQOTE',
+      '37': 'PARTIN',
+      '11': 'UTILMD',
+      '25': 'UTILTS',
+      '91': 'CONTRL',
+      '92': 'APERAK',
+      '44': 'UTILMD Gas', // UTILMD for GAS since FV2310
+      '55': 'UTILMD Strom', // UTILMD for STROM since FV2310
+    };
+
+    const key = pruefi.substring(0, 2);
+    return mapping[key];
   }
 
   onClickExport() {
