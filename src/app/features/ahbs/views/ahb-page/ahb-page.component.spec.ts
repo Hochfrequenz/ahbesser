7b8467c8f3594a763936adb82dd36ef5be30cff9
olli <144932831+OLILHR@users.noreply.github.com>
2024-08-04 15:38:20 +0200

    Add mock implementations for all new methods to ahb-page test

--- src/app/features/ahbs/views/ahb-page/ahb-page.component.spec.ts
+++ src/app/features/ahbs/views/ahb-page/ahb-page.component.spec.ts
@@ -8,13 +8,28 @@ describe('AhbPageComponent', () => {
     MockBuilder(AhbPageComponent).mock(
       AhbService,
       MockService(AhbService, {
-        getAhb: (params) =>
+        getAhb$Json: jest.fn((params) =>
           of({
             meta: {
               pruefidentifikator: params.pruefi,
+              description: 'Test description',
+              direction: 'Test direction',
+              maus_version: '1.0.0',
             },
+            lines: [],
           }),
-      } as AhbService),
+        ),
+        getAhb$Json$Response: jest.fn(),
+        getAhb$VndOpenxmlformatsOfficedocumentSpreadsheetmlSheet: jest.fn(),
+        getAhb$VndOpenxmlformatsOfficedocumentSpreadsheetmlSheet$Response:
+          jest.fn(),
+        getAhb$Csv: jest.fn(),
+        getAhb$Csv$Response: jest.fn(),
+        getFormatVersions: jest.fn(),
+        getFormatVersions$Response: jest.fn(),
+        getPruefis: jest.fn(),
+        getPruefis$Response: jest.fn(),
+      } as Partial<AhbService>),
     ),
   );
 
