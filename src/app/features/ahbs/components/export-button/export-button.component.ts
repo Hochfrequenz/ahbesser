60e18c818431093ab3b62d9ade4a22427ec95fa9
olli <info@oliverlahr.de>
2024-09-23 13:31:14 +0200

    Remove unnecessary catch block

--- src/app/features/ahbs/components/export-button/export-button.component.ts
+++ src/app/features/ahbs/components/export-button/export-button.component.ts
@@ -14,28 +14,19 @@ export class ExportButtonComponent {
   constructor(private ahbService: AhbService) {}
 
   async onClickExport(): Promise<void> {
-    try {
-      const blob = await firstValueFrom(
-        this.ahbService.getAhb$VndOpenxmlformatsOfficedocumentSpreadsheetmlSheet(
-          {
-            'format-version': this.formatVersion,
-            pruefi: this.pruefi,
-            format: 'xlsx',
-          },
-        ),
-      );
+    const blob = await firstValueFrom(
+      this.ahbService.getAhb$VndOpenxmlformatsOfficedocumentSpreadsheetmlSheet({
+        'format-version': this.formatVersion,
+        pruefi: this.pruefi,
+        format: 'xlsx',
+      }),
+    );
 
-      const url = window.URL.createObjectURL(blob);
-      const link = document.createElement('a');
-      link.href = url;
-      link.download = `AHB_${this.formatVersion}_${this.pruefi}.xlsx`;
-      link.click();
-      window.URL.revokeObjectURL(url);
-    } catch (error) {
-      console.error(
-        'No XLSX associated with this Formatversion/PruefID found in the azure blob storage.',
-        error,
-      );
-    }
+    const url = window.URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = `AHB_${this.formatVersion}_${this.pruefi}.xlsx`;
+    link.click();
+    window.URL.revokeObjectURL(url);
   }
 }
