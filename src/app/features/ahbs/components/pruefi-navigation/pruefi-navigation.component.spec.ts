cf97f871de1b1f195c8f70027153260e9b236aba
olli <info@oliverlahr.de>
2024-09-27 16:33:32 +0200

    Generate `pruefi-navigation` component

---
+++ src/app/features/ahbs/components/pruefi-navigation/pruefi-navigation.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PruefiNavigationComponent } from './pruefi-navigation.component';
+
+describe('PruefiNavigationComponent', () => {
+  let component: PruefiNavigationComponent;
+  let fixture: ComponentFixture<PruefiNavigationComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PruefiNavigationComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(PruefiNavigationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/features/ahbs/components/pruefi-navigation/pruefi-navigation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pruefi-navigation',
+  standalone: true,
+  imports: [],
+  templateUrl: './pruefi-navigation.component.html',
+  styleUrl: './pruefi-navigation.component.scss'
+})
+export class PruefiNavigationComponent {
+
+}
