74df079fa90da2c1f102daaeb67d61260c62f08a
olli <info@oliverlahr.de>
2024-10-20 16:18:26 +0200

    Test login/logout call depending on authentication state

--- src/app/shared/components/login-button/login-button.component.spec.ts
+++ src/app/shared/components/login-button/login-button.component.spec.ts
@@ -1,14 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LoginButtonComponent } from './login-button.component';
+import { AuthService } from '@auth0/auth0-angular';
+import { of } from 'rxjs';
 
 describe('LoginButtonComponent', () => {
   let component: LoginButtonComponent;
   let fixture: ComponentFixture<LoginButtonComponent>;
+  let mockAuthService: any;
 
   beforeEach(async () => {
+    mockAuthService = {
+      isAuthenticated$: of(false),
+      loginWithRedirect: jest.fn(),
+      logout: jest.fn(),
+    };
+
     await TestBed.configureTestingModule({
       imports: [LoginButtonComponent],
+      providers: [{ provide: AuthService, useValue: mockAuthService }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(LoginButtonComponent);
@@ -16,7 +25,18 @@ describe('LoginButtonComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should call loginWithRedirect when not authenticated', () => {
+    component.loginOrLogout();
+    expect(mockAuthService.loginWithRedirect).toHaveBeenCalled();
+  });
+
+  it('should call logout when authenticated', () => {
+    mockAuthService.isAuthenticated$ = of(true);
+    fixture = TestBed.createComponent(LoginButtonComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+
+    component.loginOrLogout();
+    expect(mockAuthService.logout).toHaveBeenCalled();
   });
 });
