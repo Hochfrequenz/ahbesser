c2b7c4f7925f63850b9539e2376f3487d40064ee
olli <144932831+OLILHR@users.noreply.github.com>
2024-10-20 19:18:32 +0200

    Use custom auth0 domain for prod
    Co-authored-by: kevin <68426071+hf-krechan@users.noreply.github.com>

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -3,6 +3,6 @@ import { EnvironmentInterface } from './environment.interface';
 export const environment: EnvironmentInterface = {
   apiUrl: '',
   bedingungsbaumBaseUrl: 'https://bedingungsbaum.hochfrequenz.de',
-  auth0Domain: 'hochfrequenz.eu.auth0.com',
+  auth0Domain: 'auth.hochfrequenz.de',
   auth0ClientId: 'di7tHFfTV4ZXMWXfY4DHhCcJvgJcor8k',
 };
