7df3d1f958db5d034153d62e8fca9f8864c59488
olli <info@oliverlahr.de>
2024-10-20 15:22:46 +0200

    Move Auth0 credentials to `app.config`

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,7 @@ import { routes } from './app.routes';
 import { ApiModule } from './core/api';
 import { HttpClientModule } from '@angular/common/http';
 import { environment } from './environments/environment';
+import { provideAuth0 } from '@auth0/auth0-angular';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -15,5 +16,12 @@ export const appConfig: ApplicationConfig = {
         rootUrl: environment.apiUrl,
       }),
     ),
+    provideAuth0({
+      domain: 'hochfrequenz.eu.auth0.com',
+      clientId: 'di7tHFfTV4ZXMWXfY4DHhCcJvgJcor8k',
+      authorizationParams: {
+        redirect_uri: window.location.origin,
+      },
+    }),
   ],
 };
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideAuth0 } from '@auth0/auth0-angular';
-import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, {
-  ...appConfig,
-  providers: [
-    provideAuth0({
-      domain: process.env['AUTH0_DOMAIN'] || '',
-      clientId: process.env['AUTH0_CLIENT_ID'] || '',
-      authorizationParams: {
-        redirect_uri: window.location.origin,
-      },
-    }),
-  ],
-}).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err),
+);
