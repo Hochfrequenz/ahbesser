d2dc08bf3ec9e6b172261091bb1bafa89cef810a
olli <info@oliverlahr.de>
2024-10-20 16:02:30 +0200

    Redirect to Auth0 authentication form and change button state upon login

--- src/app/shared/components/login-button/login-button.component.ts
+++ src/app/shared/components/login-button/login-button.component.ts
@@ -1,9 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
+import { CommonModule } from '@angular/common';
+import { Observable, BehaviorSubject, combineLatest } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-login-button',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './login-button.component.html',
 })
-export class LoginButtonComponent {}
+export class LoginButtonComponent implements OnInit {
+  authState$!: Observable<{ isAuthenticated: boolean; isLoading: boolean }>;
+  buttonText$ = new BehaviorSubject<string>('Einloggen');
+
+  constructor(public auth: AuthService) {}
+
+  ngOnInit() {
+    this.authState$ = combineLatest([
+      this.auth.isAuthenticated$,
+      this.auth.isLoading$,
+    ]).pipe(
+      map(([isAuthenticated, isLoading]) => ({ isAuthenticated, isLoading })),
+      tap(({ isAuthenticated, isLoading }) => {
+        if (!isLoading) {
+          this.buttonText$.next(isAuthenticated ? 'Ausloggen' : 'Einloggen');
+        }
+      }),
+    );
+  }
+
+  loginOrLogout() {
+    this.auth.isAuthenticated$.subscribe((isAuthenticated) => {
+      if (isAuthenticated) {
+        this.logout();
+      } else {
+        this.login();
+      }
+    });
+  }
+
+  private login() {
+    this.auth.loginWithRedirect();
+  }
+
+  private logout() {
+    this.auth.logout({ logoutParams: { returnTo: document.location.origin } });
+  }
+}
