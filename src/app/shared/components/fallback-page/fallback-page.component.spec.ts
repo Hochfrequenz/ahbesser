76478c39639c2882db402998e80a3ceb8b6aedf2
olli <info@oliverlahr.de>
2024-12-16 10:20:13 +0100

    Add fallback page content component

---
+++ src/app/shared/components/fallback-page/fallback-page.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FallbackPageComponent } from './fallback-page.component';
+
+describe('FallbackPageComponent', () => {
+  let component: FallbackPageComponent;
+  let fixture: ComponentFixture<FallbackPageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [FallbackPageComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(FallbackPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/shared/components/fallback-page/fallback-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fallback-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './fallback-page.component.html',
+})
+export class FallbackPageComponent {
+
+}
