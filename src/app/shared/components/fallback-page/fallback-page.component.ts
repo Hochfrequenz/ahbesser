9f71db02bad3baebc3dcd6affa481050928a24b6
olli <info@oliverlahr.de>
2024-12-16 10:21:51 +0100

    cherry pick fallback component

---
+++ src/app/shared/components/fallback-page/fallback-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fallback-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './fallback-page.component.html',
+})
+export class FallbackPageComponent {}
