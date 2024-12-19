eccb9513ce8b138f9c298b3ce674d17f9f67ac66
olli <144932831+OLILHR@users.noreply.github.com>
2024-12-19 09:48:06 +0100

    fix: move auth button into header component (#341)
    Move auth button into header component

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { IconLogoComponent } from '../icon-logo/icon-logo.component';
+import { LoginButtonComponent } from '../login-button/login-button.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, IconLogoComponent],
+  imports: [RouterLink, IconLogoComponent, LoginButtonComponent],
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {}
