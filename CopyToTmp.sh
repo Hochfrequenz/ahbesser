987b6ea20fb2a1d21aae06b9f61d4784914c908f
kevin <68426071+hf-krechan@users.noreply.github.com>
2024-12-20 17:25:49 +0100

    ci(octopus): Accelerate Deployment Process (#343)
    Copy only the pulumi directory and keep the venv folder

--- CopyToTmp.sh
+++ CopyToTmp.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-rm /tmp/ahb-tabellen -rf
-cp ./ /tmp/ahb-tabellen -r
+
+# Ensure the target directory exists
+mkdir -p /tmp/ahb-tabellen/pulumi
+
+# Remove all entries in the pulumi directory except the venv folder. This avoids the need to reinstall the python packages for each deployment.
+find /tmp/ahb-tabellen/pulumi -mindepth 1 -not -path "/tmp/ahb-tabellen/pulumi/venv*" -exec rm -rf {} +
+
+# Copy all pulumi files to the pulumi directory. We need only pulumi here, cause the application code is already in the docker image which lives in the GitHub Container Registry (GHCR).
+cp -R ./pulumi/* /tmp/ahb-tabellen/pulumi
