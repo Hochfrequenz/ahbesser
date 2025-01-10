85f67a432cd559ce0ec781b678caa14b0e2c94c5
kevin <kevin.krechan@hochfrequenz.de>
2025-01-10 12:21:57 +0100

    add apiUrl in prod env

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import { EnvironmentInterface } from './environment.interface';
 
 export const environment: EnvironmentInterface = {
   isProduction: true, // DO NOT CHANGE - disables automatic dummy user login in production environment
-  apiUrl: '',
+  apiUrl: 'https://ahb-tabellen.hochfrequenz.de',
   bedingungsbaumBaseUrl: 'https://bedingungsbaum.hochfrequenz.de',
   auth0Domain: 'auth.hochfrequenz.de',
   auth0ClientId: 'VSkXGqlTD7Rf5Q4n9a0h00rInEyL2ZQj',
