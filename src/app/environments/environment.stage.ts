38ade7341c253299e16b2760a2aa282d5597feec
kevin <kevin.krechan@hochfrequenz.de>
2025-01-10 12:22:04 +0100

    add stage env config

---
+++ src/app/environments/environment.stage.ts
@@ -0,0 +1,9 @@
+import { EnvironmentInterface } from './environment.interface';
+
+export const environment: EnvironmentInterface = {
+  isProduction: true, // DO NOT CHANGE - disables automatic dummy user login in production environment
+  apiUrl: 'https://ahb-tabellen.stage.hochfrequenz.de',
+  bedingungsbaumBaseUrl: 'https://bedingungsbaum.stage.hochfrequenz.de',
+  auth0Domain: 'auth.hochfrequenz.de',
+  auth0ClientId: 'Hku0EniRjy4B2krnx1sCwTIOzAiVta1B',
+};
