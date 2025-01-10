9b979fa4324864287ed1fef47ad99a28e373cfcf
kevin <kevin.krechan@hochfrequenz.de>
2025-01-10 18:06:11 +0100

    improve start.sh

--- start.sh
+++ start.sh
@@ -7,15 +7,11 @@ echo "Starting application setup with environment: $ENVIRONMENT"
 
 # Build the Angular application using local Angular CLI
 echo "Building Angular application..."
-./node_modules/.bin/ng build --configuration=$ENVIRONMENT
+npm run ng:build --configuration=$ENVIRONMENT
 
 # Build the Express server
 echo "Building Express server..."
 npm run server:build
 
-# switch to non-root user nodejs
-su - nodejs
-
 # Start the server
-echo "Starting the Express server..."
 node dist/server/server.js
