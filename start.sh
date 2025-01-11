e6b7fec808f99f46d582a69570e06a2bb129a20b
kevin <kevin.krechan@hochfrequenz.de>
2025-01-11 21:55:28 +0100

    Add build command to echo

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ set -e
 echo "Starting application setup with environment: $ENVIRONMENT"
 
 # Build the Angular application using local Angular CLI
-echo "Building Angular application..."
+echo "Building Angular application with the command 'npm run ng:build --configuration=$ENVIRONMENT'..."
 npm run ng:build --configuration=$ENVIRONMENT
 
 # Build the Express server
