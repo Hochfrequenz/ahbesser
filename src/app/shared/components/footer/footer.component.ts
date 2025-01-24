84e10d364d73d1ab2d7d6eadad468920afbae325
kevin <kevin.krechan@hochfrequenz.de>
2025-01-24 18:48:39 +0100

    fix linter errors

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { IconLogoComponent } from '../icon-logo/icon-logo.component';
+import { CommonModule } from '@angular/common';
 import { IconHeartComponent } from '../icon-heart/icon-heart.component';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [IconHeartComponent],
+  imports: [CommonModule, IconHeartComponent],
   templateUrl: './footer.component.html',
 })
 export class FooterComponent {
