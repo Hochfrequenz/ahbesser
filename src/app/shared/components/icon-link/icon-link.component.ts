8e807c30926177f89edba8688e520476f7d5a023
olli <info@oliverlahr.de>
2025-01-25 18:55:56 +0100

    Add link icon component

---
+++ src/app/shared/components/icon-link/icon-link.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-icon-link',
+  standalone: true,
+  imports: [],
+  templateUrl: './icon-link.component.html',
+})
+export class IconLinkComponent {}
