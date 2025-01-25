93256ec4cce99c7e9b7e1185d98e41af27de1af8
konstantin <konstantin.klein@hochfrequenz.de>
2025-01-25 10:23:24 +0100

    chore: add environment variable `ebdBaseUrl` to config (#394)
    ---------
    
    Co-authored-by: Konstantin <konstantin.klein+github@hochfrequenz.de>

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -6,6 +6,7 @@ export const environment: EnvironmentInterface = {
   isProduction: false, // during local development (localhost 4000/4200), the user "local@development.com" is authenticated by default
   apiUrl: 'http://localhost:3000',
   bedingungsbaumBaseUrl: 'https://bedingungsbaum.stage.hochfrequenz.de',
+  ebdBaseUrl: 'https://ebd.stage.hochfrequenz.de',
   auth0Domain: 'auth.hochfrequenz.de',
   auth0ClientId: 'Hku0EniRjy4B2krnx1sCwTIOzAiVta1B',
 };
