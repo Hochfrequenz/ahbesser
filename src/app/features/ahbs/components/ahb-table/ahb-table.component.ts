09408c4c93145b1c14d2e8df9cd977bc1cf2e373
Konstantin <konstantin.klein+github@hochfrequenz.de>
2025-01-26 12:48:29 +0100

    prettier

--- src/app/features/ahbs/components/ahb-table/ahb-table.component.ts
+++ src/app/features/ahbs/components/ahb-table/ahb-table.component.ts
@@ -161,7 +161,7 @@ export class AhbTableComponent {
     return `${environment.bedingungsbaumBaseUrl}/tree/?format=${this.formatVersion()}&format_version=${this.getFormat(this.pruefi())}&expression=${encodedExpression}`;
   }
   generateEbdDeepLink(value_pool_entry: string | null): string | null {
-    if (!value_pool_entry||value_pool_entry.trim().length === 0) {
+    if (!value_pool_entry || value_pool_entry.trim().length === 0) {
       return null;
     }
     const regex = /^.*\b(?<ebd_key>E_\d+)\b.*$/;
