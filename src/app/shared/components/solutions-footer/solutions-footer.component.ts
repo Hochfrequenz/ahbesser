0ecdc5bf524d93f76fd1f96b20b4bd35e369640e
kevin <kevin.krechan@hochfrequenz.de>
2025-02-18 22:23:37 +0100

    split two footers

---
+++ src/app/shared/components/solutions-footer/solutions-footer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-solutions-footer',
+  imports: [],
+  templateUrl: './solutions-footer.component.html',
+  styleUrl: './solutions-footer.component.scss'
+})
+export class SolutionsFooterComponent {
+
+}
