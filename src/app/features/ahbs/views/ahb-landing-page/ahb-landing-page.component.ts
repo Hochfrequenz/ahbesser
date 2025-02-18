f85437cccadcad780df67caac9aaa8006728f2a7
kevin <kevin.krechan@hochfrequenz.de>
2025-02-18 19:49:15 +0100

    add hint for prüfi input
    still wip

---
+++ src/app/features/ahbs/views/ahb-landing-page/ahb-landing-page.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../../../../shared/components/header/header.component';
+import { FooterComponent } from '../../../../shared/components/footer/footer.component';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { FormatVersionSelectComponent } from '../../components/format-version-select/format-version-select.component';
+import { PruefiInputComponent } from '../../components/pruefi-input/pruefi-input.component';
+import { AhbSearchFormHeaderComponent } from '../../components/ahb-search-form-header/ahb-search-form-header.component';
+import { InputSearchEnhancedComponent } from '../../../../shared/components/input-search-enhanced/input-search-enhanced.component';
+
+@Component({
+  selector: 'app-ahb-landing-page',
+  standalone: true,
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    FormsModule,
+    ReactiveFormsModule,
+    FormatVersionSelectComponent,
+    PruefiInputComponent,
+    AhbSearchFormHeaderComponent,
+    InputSearchEnhancedComponent,
+  ],
+  templateUrl: './ahb-landing-page.component.html',
+})
+export class AhbLandingPageComponent {
+  form = new FormGroup({
+    formatVersion: new FormControl<string>('', {
+      nonNullable: true,
+      validators: [Validators.required],
+    }),
+    pruefi: new FormControl<string>('', { nonNullable: true, validators: [Validators.required] }),
+  });
+
+  constructor(private readonly router: Router) {}
+
+  onClickSubmit() {
+    if (!this.form.valid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+    this.router.navigate(['/ahb', this.form.value.formatVersion, this.form.value.pruefi]);
+  }
+}
