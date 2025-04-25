7182cc25f0d6eefd38be3e6aa94932a12308d01b
kevin <kevin.krechan@hochfrequenz.de>
2025-04-25 13:09:12 +0200

    add hint where the sqlite comes from

--- src/server/entities/ahb-line.entity.ts
+++ src/server/entities/ahb-line.entity.ts
@@ -1,6 +1,7 @@
 // The here defined columns come from the ahbline table in the database.
 // The table is created by the ahbline.sql file in the database/sql folder.
-// Which you can find here: https://github.com/Hochfrequenz/ahb-mig-backend/releases/tag/v0.0.1
+// Which you can find here: https://github.com/Hochfrequenz/xml-fundamend-python/blob/main/src/fundamend/sqlmodels/create_ahbtabellen_view.sql
+// The related SQLModel can by found here: https://github.com/Hochfrequenz/xml-fundamend-python/blob/9a45e07f953d42c707db4c7af4294790f26e01ce/src/fundamend/sqlmodels/ahbtabellen_view.py#L37-L60
 
 import { Entity, Column, PrimaryColumn } from 'typeorm';
 
